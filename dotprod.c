ae378dd402cf4b1615fda8030a7945d361c02f44
Wol Bol Wol Majok <majokw@whitman.edu>
2025-09-29 13:22:56 -0700

    DotProduct implementation

--- dotprod.c
+++ dotprod.c
@@ -1,6 +1,15 @@
-int main {
+#include <stdio.h>
+int main(){
     int dotProduct = 0;
     int firstVector[3], secondVector[3];
     printf("Enter 3 components of tyhe first vector");
-    scanf("%d %d % d", &firstVector[0], &firstVector[1],&firstVector[2])
+    scanf("%d %d % d", &firstVector[0], &firstVector[1],&firstVector[2]);
+
+    printf("Enter 3 components of tyhe second vector");
+    scanf("%d %d % d", &secondVector[0], &secondVector[1],&secondVector[2]);
+
+    for(int i = 0; i < 3; i++){
+       dotProduct +=   firstVector[i] * secondVector[i]; 
+    }
+    printf("DotProduct is: %d", dotProduct);
 }
