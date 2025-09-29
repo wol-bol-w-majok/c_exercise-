5d7585fd8a5618d837ff0f0b13bdaaed217f074b
Wol Bol Wol Majok <majokw@whitman.edu>
2025-09-29 13:14:52 -0700

    First vector implementation

---
+++ dotprod.c
@@ -0,0 +1,6 @@
+int main {
+    int dotProduct = 0;
+    int firstVector[3], secondVector[3];
+    printf("Enter 3 components of tyhe first vector");
+    scanf("%d %d % d", &firstVector[0], &firstVector[1],&firstVector[2])
+}
